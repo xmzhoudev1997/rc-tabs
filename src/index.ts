22491c9b9c632f13f1aa8184d1b6205e59bf32c6
xmzhoudev1997 <165015145+xmzhoudev1997@users.noreply.github.com>
2024-04-01 09:59:58 +0800

    删除代码文件

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as TabsNav } from './tabs-nav';
-export { default as TabsPanel } from './tabs-panel';
-export { default as useTabsNav } from './hooks/use-tabs-nav';
-export * from './tabs-nav/props';
-export * from './tabs-panel/props';
